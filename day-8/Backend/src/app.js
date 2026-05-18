47a2edcfbed0d0e932c9ba61e7a2bd3a0402c99b
Ananya <workwithananya20@gmail.com>
2026-05-18 18:07:38 +0530

    day-8

---
+++ day-8/Backend/src/app.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const noteModel=require("./models/notes.model.js")
+const app=express();
+app.use(express.json())
+app.post("/notes", async (req,res)=>{
+     const{title,Description}=req.body;
+     const note= await noteModel.create({
+         title,Description
+     })
+     res.status(201).json({
+        message:"note created successfully",
+        note
+     })
+})
+app.get("/notes", async (req,res)=>{
+     const notes = await noteModel.find()
+     res.status(200).json({
+        message:"notes fetched successfully",
+        notes
+     })
+})
+app.delete("/notes/:id", async (req,res)=>{
+     const id=req.params.id
+     await noteModel.findByIdAndDelete(id);
+     res.status(200).json({
+        message:"note deleted successfully"
+     })
+})
+app.patch("/notes/:id", async (req,res)=>{
+      const id=req.params.id;
+      const{Description}=req.body;
+     const updatednote=await noteModel.findByIdAndUpdate(id,{Description})
+     res.status(200).json({
+        message:"note updated successfully",
+        updatednote
+     })
+})
+module.exports=app;
